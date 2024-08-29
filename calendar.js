7cc73150688739c6a6af47d4a1f8ba19614fda89
Baksa Krisztián <baksa.krisztian@hmzrinyi.hu>
2024-08-29 14:49:55 +0200

    Léptetés, minimális design, hotfix-ek

--- calendar.js
+++ calendar.js
@@ -7,15 +7,60 @@ class Calendar {
 
     constructor() {
         let date = new Date();
-        this.viewedYear = 2025;//date.getFullYear();
-        this.viewedMonth = 1;//date.getMonth() + 1;
+        this.viewedYear = date.getFullYear();
+        this.viewedMonth = date.getMonth() + 1;
         this.events = [];
+
+        this.assignEvents();
+    }
+
+    assignEvents(){
+
+        let prevMonthButton = document.querySelector('.prev-month');
+
+        if(prevMonthButton){
+            prevMonthButton.addEventListener('click', () => {
+                
+                this.stepBackward();
+            });
+        }
+
+        let nextMonthButton = document.querySelector('.next-month');
+
+        if(nextMonthButton){
+            nextMonthButton.addEventListener('click', () => {
+
+                this.stepForward();
+            });
+        }
+    }
+
+    stepForward(){
+        this.viewedMonth++;
+        console.log(this.viewedMonth);
+        if(this.viewedMonth > 12){
+            this.viewedMonth = 1;
+            this.viewedYear++;
+        }
+        this.render();
+    }
+
+    stepBackward(){
+        this.viewedMonth--;
+        if(this.viewedMonth < 1){
+            this.viewedMonth = 12;
+            this.viewedYear--;
+        }
+        this.render();
     }
 
     monthlyView(){
-        console.log(`Month: ${this.viewedMonth}, Year: ${this.viewedYear}`);
+        let currentMonthNumber = (this.viewedMonth /*+ 1*/);
+
+        console.log(`Month: ${currentMonthNumber}, Year: ${this.viewedYear}`);
+
         // Generate calendar for the given month
-        let dateToShow = new Date(this.viewedYear, (this.viewedMonth - 1),1);
+        let dateToShow = new Date(this.viewedYear, (currentMonthNumber - 1),1);
         let dayOfTheWeek = dateToShow.getDay() == 0 ? 7 : dateToShow.getDay(); //Def: 0 - 6
 
         console.log(`Weekday of current month's FIRST: ${dayOfTheWeek}`);
@@ -24,9 +69,9 @@ class Calendar {
         viewData[0] = [];   //First week
 
         /**
-         * =========== PREV months last days
+         * =========== PREV month
          */ 
-        let prevMonth = (this.viewedMonth - 1);
+        let prevMonth = (currentMonthNumber - 1);
         prevMonth = prevMonth == 0 ? 12 : prevMonth;
         let prevMonthsYear = prevMonth == 12 ? this.viewedYear - 1 : this.viewedYear;
 
@@ -36,7 +81,6 @@ class Calendar {
         let iPrev = (prevMonthDayCount - (dayOfTheWeek - 2));
 
         while(iPrev <= prevMonthDayCount){
-            console.log(iPrev + "Prev MONT");
             viewData[0].push(this.makeDayObject(iPrev, "buffer"));
             iPrev++;
         }
@@ -45,7 +89,7 @@ class Calendar {
          * =========== CURRENT month
          */
         let currentWeekIndex = 0;
-        let dateToShowDayCount = new Date(this.viewedYear, this.viewedMonth,0);
+        let dateToShowDayCount = new Date(this.viewedYear, currentMonthNumber,0);
         let dayIndex = (dayOfTheWeek - 1);
 
         for(let iCurr = 1; iCurr <= dateToShowDayCount.getDate(); iCurr++) {
@@ -65,17 +109,79 @@ class Calendar {
          * =========== NEXT month
          */
         let lastWeekCount = viewData[viewData.length - 1].length;
-
         let bufferDays = 1;
 
         while(lastWeekCount < 7){
-            console.log(lastWeekCount + " Next MONT");
             viewData[viewData.length - 1].push(this.makeDayObject(bufferDays, "buffer"));
             bufferDays++;
             lastWeekCount++;
         }
 
         console.log(viewData);
+
+        return viewData;
+    }
+
+    render() {
+        /** Header */
+        document.querySelector(".weekdays").innerHTML = "";
+
+        let date = new Date(this.viewedYear, (this.viewedMonth - 1), 1);
+        let monthName = date.toLocaleString('default', { month: 'long' });
+
+        document.querySelector('#month-year').innerHTML = this.viewedYear + " " + monthName;
+
+        // Localized Weekday names
+        for(let i = 1; i <= 7; i++) {
+
+            const dummyDate = new Date(2001, 0, i);
+            const locale = new Intl.DateTimeFormat().resolvedOptions().locale;
+            let weekDayName = dummyDate.toLocaleDateString(locale, { weekday: 'long' });
+
+            let dayCell = document.createElement("div");
+            dayCell.classList.add("weekday");
+            dayCell.textContent = weekDayName;
+            document.querySelector(".weekdays").appendChild(dayCell);
+        }
+
+        /** Content */
+        this.renderContent();
+    }
+
+    renderContent() {
+        
+        let contentObject = document.querySelector(".calendar-content");
+
+        if(contentObject){
+
+            contentObject.innerHTML = "";
+
+            let calendarData = this.monthlyView();
+
+            calendarData.forEach((week, index) => {
+
+                let weekRow = document.createElement("div");
+                weekRow.classList.add("week");
+                
+                week.forEach(day => {
+                    let dayCell = document.createElement("div");
+                    dayCell.classList.add("day");
+                    dayCell.setAttribute("day", day.day);
+                    
+                    if(day.type == "buffer"){
+                        dayCell.classList.add("buffer");
+                    }
+
+                    dayCell.textContent = day.day;
+
+                    weekRow.appendChild(dayCell);
+
+                    //console.log(`Day: ${day.day}, Type: ${day.type}, Events: ${day.events.length}`);
+                });
+
+                document.querySelector(".calendar-content").appendChild(weekRow);
+            });
+        }
     }
 
     makeDayObject(day, type){
